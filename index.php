228d2f6994ab602ae43693225f3fffcd99a04691
Sergei Sokolov <sokolov12ssa@gmail.com>
2023-05-03 22:22:06 +0500

    Setters & Getters

--- index.php
+++ index.php
@@ -2,42 +2,72 @@
 // Create a person class
 class Person
 {
+    // Define private properties
     private $_name;
     private $_age;
 
-    // Constructor runs automatically when an object has been created
+    // Constructor runs automatically when the class has been instantiated
     public function __construct($name, $age)
     {
-        echo __CLASS__ . ' ' . 'has been instantiated <br>';
+        echo 'Class ' . __CLASS__ . ' ' . 'has been instantiated <br>';
         $this->_name = $name;
         $this->_age = $age;
     }
 
-    // Destructor runs automatically when there's no reference to an objec, used for cleanup, closing database connections etc
-
+    // Destructor run automatically when there's no reference to an object, when doing cleanup, closing connections to a database
     public function __destruct()
     {
-        echo 'Destructor ran...';
-        echo '<br>';
+        echo 'Destructor ran... <br>';
     }
 
-    // Say Hello function
-    public function sayHello()
+    // Greeting method
+    public function greeting()
     {
-        return 'Hello ' . $this->_name . '<br>' . 'You are ' . $this->_age;
+        return 'Hello ' . $this->_name . '! ' . 'You are ' . $this->_age . ' ' . 'years old';
+    }
+
+    // Get name
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    // Set name
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+    // __get MAGIC METHOD
+    public function __get($property)
+    {
+        // Check if the property exists
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    // __set MAGIC METHOD
+    public function __set($property, $value)
+    {
+        // Check if the property exists
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+        return $this;
     }
 }
 
 // Instantiate Person class
 $person1 = new Person('Sergei', 39);
-echo $person1->sayHello();
-
+echo $person1->greeting();
 echo '<br>';
-
 $person2 = new Person('Julia', 25);
-echo $person2->sayHello();
+echo $person2->greeting();
+echo '<br>';
 
-echo '<br>';
-$person3 = new Person('Daniil', 8);
-echo $person3->sayHello();
-echo '<br>';
+// $person1->setName('Alexander');
+// echo $person1->getName();
+// echo '<br>';
+$person1->__set('_age', 40);
+echo $person1->__get('_age');
